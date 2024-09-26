a1bde6b700bf0b969b64469c8f26c7888e3c1869
iisamontoya <isabella.montoyag@upb.edu.co>
2024-09-26 09:55:30 -0500

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -26,6 +26,10 @@ Scanner scanner = new Scanner(System.in);
                 resultado = num1 - num2;
                 System.out.println("Resultado: " + resultado);
                 break;
+        case '*':
+                resultado = num1 * num2;
+                System.out.println("Resultado: " + resultado);
+                break;
 
 
 
