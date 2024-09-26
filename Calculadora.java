a7b55507db715fee18568ec5b0e10e31970be934
SimonP0607 <cnc.simon.perez.6a@gmail.com>
2024-09-26 09:38:54 -0500

    Menu calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -1,1 +1,27 @@
+public class Calculadora {
 
+public static void main(String[] args) {
+
+Scanner scanner = new Scanner(System.in);
+
+        // Pedir al usuario los dos números
+        System.out.print("Ingresa el primer número: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.print("Ingresa el segundo número: ");
+        double num2 = scanner.nextDouble();
+        
+        // Pedir al usuario la operación a realizar
+        System.out.print("Ingresa la operación (+, -, *, /): ");
+        char operacion = scanner.next().charAt(0);
+        
+        double resultado;
+
+
+
+
+}
+
+
+
+}
